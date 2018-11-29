d8a5d738571a3b0082ca4885759c54e1dfed03d7
JUNG Myoungki <myoungki.jung@gmail.com>
2018-11-29 18:04:59 +1000

    OOP in Functional Programming style
    More like trait based

--- OPP_factory.cpp
+++ OPP_factory.cpp
@@ -22,10 +22,12 @@ class VehiclePart { };
 class Wheel:public VehiclePart { };
 class CarWheel:public Wheel { };
 class TrainWheel:public Wheel { };
+class AirPlaneWheel:public Wheel { };
 
 class Motor:public VehiclePart { };
 class CarMotor:public Motor { };
 class TrainMotor:public Motor { };
+class AirPlaneMotor:public Motor { };
 
 class VehiclePartFactory
 {
@@ -60,15 +62,88 @@ public:
   }
 };
 
-void UseFactory (const VehiclePartFactory & factory)
+void
+UseFactory (const VehiclePartFactory & factory)
 {
   const auto motor = factory.CreateMotor ();
   const auto leftWheel = factory.CreateWheel ();
   // ...
 }
 
-int main ()
+/********************************************************************************
+ * functional programming version
+ */
+enum class vehicle_type
+{ Car, Train, AirPlane };
+enum class part_type
+{ Motor, Wheel };
+
+std::unique_ptr < VehiclePart > create_part (vehicle_type v_t, part_type p_t)
 {
+  using namespace std;
+  switch (v_t)
+    {
+    case vehicle_type::Train:
+      switch (p_t)
+	{
+	case part_type::Motor:
+	  return std::make_unique < TrainMotor > ();
+	  break;
+	case part_type::Wheel:
+	  return std::make_unique < TrainWheel > ();
+	  break;
+	}
+    case vehicle_type::AirPlane:
+      switch (p_t)
+	{
+	case part_type::Motor:
+	  return std::make_unique < AirPlaneMotor > ();
+	  break;
+	case part_type::Wheel:
+	  return std::make_unique < AirPlaneWheel > ();
+	  break;
+	}
+    case vehicle_type::Car:
+      switch (p_t)
+	{
+	case part_type::Motor:
+	  return std::make_unique < CarMotor > ();
+	  break;
+	case part_type::Wheel:
+	  return std::make_unique < CarWheel > ();
+	  break;
+	}
+    }
+}
+
+const auto carPartFactory =
+  fplus::bind_1st_of_2 (create_part, vehicle_type::Car);
+const auto trainPartFactory =
+  fplus::bind_1st_of_2 (create_part, vehicle_type::Train);
+const auto airPlanePartFactory =
+  fplus::bind_1st_of_2 (create_part, vehicle_type::AirPlane);
+
+typedef
+  std::function < std::unique_ptr < VehiclePart > (part_type) > Factory;
+
+void UseFunctionalFactory (const Factory & factory)
+{
+  const auto
+    motor = factory (part_type::Motor);
+  const auto
+    leftWheel = factory (part_type::Wheel);
+}
+
+int
+main ()
+{
+  using namespace
+    fplus;
+
   UseFactory (CarPartFactory ());
   UseFactory (TrainPartFactory ());
+
+  UseFunctionalFactory (carPartFactory);
+  UseFunctionalFactory (trainPartFactory);
+  UseFunctionalFactory (airPlanePartFactory);
 }
